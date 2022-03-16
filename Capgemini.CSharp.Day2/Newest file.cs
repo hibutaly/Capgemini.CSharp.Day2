7943045ad714014e98c66ba9b72b6c273889020e
hibutaly <hibu1729@gmail.com>
2022-03-16 12:34:02 +0530

    new added branch

---
+++ Capgemini.CSharp.Day2/Newest file.cs
@@ -0,0 +1,11 @@
+using System;
+namespace Capgemini
+{
+    public class polio
+    {
+        static void Main(string[] args)
+        {
+                System.Console.WriteLine(   "hi");
+        }
+    }
+}
