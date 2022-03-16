f6d959a4d1933422ec770eaa7ffc3e9aae0da65a
hibutaly <hibu1729@gmail.com>
2022-03-16 12:19:20 +0530

    FasrtCommint

---
+++ Capgemini.CSharp.Day2/EqualsExample.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capgemini.CSharp.Day2
+{
+    internal class EqualsExample
+    {
+        static void Main(string[] args)
+        {
+
+            //equals - checks only value,,,,,,==- checks the refernce memory
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Capgemini.CSharp.Day2/StringMethod.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using static System.Console;
+
+namespace Capgemini.CSharp.Day2
+{
+    internal class StringMethod
+    {
+        static void Main(string[] args)
+        {
+            string str = "        Capgemini             ";
+            //WriteLine($"length:{str.Length}");
+
+            ////clone
+            //string s =str.Clone() as string;
+            //WriteLine($"clone :{s}");
+            //WriteLine($"remove left space:{str.TrimStart()}{str.TrimEnd()}");
+
+            //string str1 = "Good Morning Everyone";
+            //string[] test=str1.Split('o');
+            ////foreach (string test2 in test)
+            ////{
+            ////    WriteLine(test2);
+            ////}
+
+
+            //WriteLine($"join string:{string.Join('p', test)}");
+
+
+            //substring
+
+            string pid = "p00001";
+            int id = int.Parse(pid.Substring(1, 5));
+            id++;
+            WriteLine($"new id:{"p000" + id}");
+            WriteLine($"id is{ id}");
+            WriteLine(pid + 1);
+
+
+            WriteLine(str.Substring(3, 6));
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
